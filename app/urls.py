0f757d73ba42873d1e71220fc3fdaa92e08446c1
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-02 21:58:59 +0330

    change sum api view name

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from .views import (
-    CalculateParamsAPIView,
+    CalculateSumParamsAPIView,
     GetParamsHistoryAPIView,
     GetTotalParamsAPIView
 )
 
 urlpatterns = [
-    path('sum/', CalculateParamsAPIView.as_view(), name='sum'),
+    path('sum/', CalculateSumParamsAPIView.as_view(), name='sum'),
     path('history/', GetParamsHistoryAPIView.as_view(), name='history'),
     path('total/', GetTotalParamsAPIView.as_view(), name='total'),
 ]
