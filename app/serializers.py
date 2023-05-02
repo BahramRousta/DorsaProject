91de1b1dd69c0fe0ef703815607714b6afcbfee8
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-02 21:05:03 +0330

    add ParamsHistorySerializer and TotalParametersSerializer

--- app/serializers.py
+++ app/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from .models import Parameter
 
 
 class ParametersSerializer(serializers.Serializer):
@@ -6,3 +7,19 @@ class ParametersSerializer(serializers.Serializer):
 
     a = serializers.FloatField()
     b = serializers.FloatField()
+
+
+class ParamsHistorySerializer(serializers.ModelSerializer):
+    """Serializer for parameters history"""
+
+    class Meta:
+        model = Parameter
+        fields = ('a', 'b')
+
+
+class TotalParametersSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Parameter
+        fields = ('total',)
+
