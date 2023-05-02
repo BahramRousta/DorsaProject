25d3762c8aa5fffb3728e017a7ed36ae6910aaf0
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-02 21:05:24 +0330

    add history and total urls

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import CalculateParamsAPIView
+from .views import (
+    CalculateParamsAPIView,
+    GetParamsHistoryAPIView,
+    GetTotalParamsAPIView
+)
 
 urlpatterns = [
     path('sum/', CalculateParamsAPIView.as_view(), name='sum'),
+    path('history/', GetParamsHistoryAPIView.as_view(), name='history'),
+    path('total/', GetTotalParamsAPIView.as_view(), name='total'),
 ]
