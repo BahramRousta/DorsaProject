1b487b16806ed029d7d00004864bf8426454496b
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-02 20:13:15 +0330

    add calculate params api view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import ParametersSerializer
+from .models import Parameter
 
-# Create your views here.
+
+class CalculateParamsAPIView(APIView):
+    """Parameters API view."""
+    serializer_class = ParametersSerializer
+
+    def get(self, request):
+
+        serializer = self.serializer_class(data=request.query_params)
+        serializer.is_valid(raise_exception=True)
+
+        # Get parameters
+        a = serializer.data.get('a', None)
+        b = serializer.data.get('b', None)
+
+        # Calculate the parameter
+        total = (a + b)
+
+        # Create new parameters
+        Parameter.objects.create(params={
+            "a": a,
+            "b": b,
+            "total": total
+        })
+
+        return Response({'result': total})
