dcc70831cdaa19ccd21f86209b3d8888f3d35cbd
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-02 21:04:38 +0330

    change __str__ of Parameter model

--- app/models.py
+++ app/models.py
@@ -4,7 +4,10 @@ from django.db import models
 class Parameter(models.Model):
     """Model for parameters."""
 
-    params = models.JSONField(default=dict)
+    a = models.FloatField()
+    b = models.FloatField()
+    total = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.params
+        return f"{self.a} + {self.b} = {self.total}"
