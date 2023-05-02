010a3667de2e3d4bb9a0b92ed78c91ba11d68742
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-02 20:11:53 +0330

    change model to save parameters as JSON

--- app/models.py
+++ app/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+
+class Parameter(models.Model):
+    """Model for parameters."""
+
+    params = models.JSONField(default=dict)
+
+    def __str__(self):
+        return self.params
