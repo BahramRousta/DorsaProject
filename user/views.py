59cd0c7cb06ead3cc44c69962f2f85aaae13e1ef
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-03 11:16:50 +0330

    add login api view

--- user/views.py
+++ user/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from user.models import CustomUser
+from user.serializers import LoginSerializer, ObtainTokenSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+
+class LoginAPIView(APIView):
+    """Login API View. Receive username and password. Return access and refresh token."""
+    serializer_class = LoginSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def _obtain_token(self, request, user):
+        """Return access and refresh token for user."""
+
+        refresh = RefreshToken.for_user(user)
+        token = ObtainTokenSerializer({
+            'refresh_token': str(refresh),
+            'access_token': str(refresh.access_token)
+        }).data
+        return token
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+
+            # Get user from DB
+            user = CustomUser.objects.filter(username=data["username"]).first()
+
+            # Validate password
+            if user and user.check_password(data["password"]):
+                login(request, user)
+                return Response(status=status.HTTP_200_OK, data=self._obtain_token(request, user))
+            else:
+                return Response(status=status.HTTP_403_FORBIDDEN, data={"Message": "The information is invalid."})
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
