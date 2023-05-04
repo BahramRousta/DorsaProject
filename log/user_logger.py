6e2359b7121c882fb09ef6721193a8cadbb89065
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-04 16:15:27 +0330

    change logfile dir

--- log/user_logger.py
+++ log/user_logger.py
@@ -4,7 +4,7 @@ import logging
 user_logger = logging.getLogger(__name__)
 user_logger.view_name = None
 user_logger.setLevel(logging.INFO)
-user_filehandler = logging.FileHandler(filename='user.log', mode='a')
+user_filehandler = logging.FileHandler(filename='log/user.log', mode='a')
 user_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 user_filehandler.setFormatter(user_formatter)
 user_logger.addHandler(user_filehandler)
