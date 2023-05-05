b3e4eade6f1f9a70b0642d0e833465ff51acd46d
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-05-05 16:19:08 +0330

    add params query throttle unit test

--- tests/test_app/test_throttles.py
+++ tests/test_app/test_throttles.py
@@ -32,3 +32,33 @@ class TestThrottles:
 
         response = api_client.post(url)
         assert response.status_code == status.HTTP_429_TOO_MANY_REQUESTS
+
+    @pytest.mark.django_db
+    def test_data_wrong_method_throttle(self, api_client, invalid_data, user_factory):
+        """
+        Test that the CalculateSum API view is throttled
+        """
+        user = user_factory
+        api_client.force_authenticate(user)
+        url = reverse('sum')
+
+        for i in range(15):
+            api_client.get(url, params=invalid_data)
+
+        response = api_client.post(url, params=invalid_data)
+        assert response.status_code == status.HTTP_429_TOO_MANY_REQUESTS
+
+    @pytest.mark.django_db
+    def test_query_params_wrong_method_throttle(self, api_client, parameters_data, user_factory):
+        """
+        Test that the CalculateSum API view is throttled
+        """
+        user = user_factory
+        api_client.force_authenticate(user)
+        url = reverse('total')
+
+        for i in range(15):
+            api_client.post(url, parameters_data)
+
+        response = api_client.post(url)
+        assert response.status_code == status.HTTP_429_TOO_MANY_REQUESTS
